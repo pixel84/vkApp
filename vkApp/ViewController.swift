88ad76316a2b2a2d139ea23427cc4490e0ad27ff
Alex Shader <pixel84@gmail.com>
2020-05-20 14:42:07 +0300

    Initial Commit

---
+++ vkApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vkApp
+//
+//  Created by Alex Shader on 20.05.2020.
+//  Copyright © 2020 shaderness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
