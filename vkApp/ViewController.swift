59a1085fcf7f701ae3b3ed1507f9ed93689d6eec
Alex Shader <pixel84@gmail.com>
2020-05-20 15:53:41 +0300

    code 1

--- vkApp/ViewController.swift
+++ vkApp/ViewController.swift
@@ -9,11 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
-
+    
+    @IBAction func buttonPressed() {
+        let login = loginInput.text!
+        let password = passwordInput.text!
+        if login == "admin" && password == "123456" {
+            print("Access Granted")
+        } else {
+            print("Access Denied")
+        }
+    }
+    
+    @objc func keyboardUp(notification: Notification) {
+        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
+        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        scrollBottomConstraint.constant = frame.height
+        
+    }
+    @objc func keyboardDown(notification: Notification) {
+        scrollBottomConstraint.constant = 0
+        
+    }
+    
 }
 
