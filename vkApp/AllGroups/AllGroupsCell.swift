d35c1303e95876d06d8dde1b79b85d592d526952
Alex Shader <pixel84@gmail.com>
2020-06-02 16:10:57 +0300

    не видно группы

---
+++ vkApp/AllGroups/AllGroupsCell.swift
@@ -0,0 +1,27 @@
+//
+//  AllGroupsCell.swift
+//  vkApp
+//
+//  Created by Alex Shader on 02.06.2020.
+//  Copyright © 2020 shaderness. All rights reserved.
+//
+
+import UIKit
+
+class AllGroupsCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var groupCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
